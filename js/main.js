aea88fccfa7541e1a4f967a61c27cfbea3f789d6
Álvaro Martín <62563198+Varoso@users.noreply.github.com>
2022-06-09 19:10:45 +0200

    Content modifications and initial backdrop

---
+++ js/main.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    openBackdrop();
+};
+
+function openBackdrop() {
+  setTimeout(function () {
+    backdropStatus = document.getElementById("backdrop-status");
+    backdropStatus.checked = false;
+  }, 1500);
+}
