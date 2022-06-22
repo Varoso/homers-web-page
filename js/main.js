e863f852c381ade7b306ebec936898c84ab1cf5f
Varoso <varomartin@icloud.com>
2022-06-22 20:12:53 +0200

    Added initial support for multilanguage and multiversion

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 window.onload = function() {
-    openBackdrop();
+  setDefaultData();
+  setContent();
+  openBackdrop();
 };
 
 function openBackdrop() {
@@ -8,3 +10,47 @@ function openBackdrop() {
     backdropStatus.checked = false;
   }, 1500);
 }
+
+function setDefaultData() {
+  if(!window.localStorage.getItem('version')) {
+    window.localStorage.setItem('version', 'v1');
+  }
+  if(!window.localStorage.getItem('language')) {
+    window.localStorage.setItem('language', 'english');
+  }
+}
+
+function setContent() {
+  let language;
+  switch(window.localStorage.getItem('language')) {
+    case 'english':
+      language = english;
+      break;
+    case 'spanish':
+      language = spanish;
+      break;
+  }
+
+  document.title = language.title;
+  document.getElementById('header').textContent = language.header;
+  document.getElementById('backdrop-text-left').textContent = language.backdropTextLeft;
+  document.getElementById('backdrop-text-right').textContent = language.backdropTextRight;
+}
+
+var english = {
+  title: "Homer's Web Page",
+  backdropTextLeft: "Homer's",
+  backdropTextRight: "Web Page",
+  header: "Homer's Web Page",
+  version: "Version",
+  language: "Language"
+};
+  
+var spanish = {
+  title: "Página web de Homer",
+  backdropTextLeft: "Página web",
+  backdropTextRight: "de Homer",
+  header: "Página web de Homer",
+  version: "Versión",
+  language: "Idioma"
+};
